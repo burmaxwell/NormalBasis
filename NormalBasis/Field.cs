245a0d268cff287a73eb326d0a88ce10220eb792
administrator <burmax98@gmail.com>
2019-01-03 11:27:58 +0200

    Add with tests

--- NormalBasis/Field.cs
+++ NormalBasis/Field.cs
@@ -8,5 +8,54 @@ namespace NormalBasis
 {
     class Field
     {
+        public static int[] String_To_Byte(string stringbitvector)
+        {
+            var bitlenth = stringbitvector.Length;
+            int[] number = new int[bitlenth];
+
+            for (var i = bitlenth-1; i>=0; i--)
+            {
+                number[i] = Convert.ToByte(stringbitvector.Substring(stringbitvector.Length - (i + 1), 1), 2);
+            }
+
+            return number;
+        }
+
+        public static string Byte_To_String(int[] bitvector)
+        {
+            StringBuilder stringline = new StringBuilder();
+
+            for (int i = bitvector.Length - 1; i >= 0; i--)
+            {
+                stringline.Append(Convert.ToString(bitvector[i], 2));
+            }
+
+            return stringline.ToString();
+        }
+
+        public static int[] Add(int[] a, int[] b)
+        {
+            var maxlenght = Math.Max(a.Length, b.Length);
+
+            Array.Resize(ref a, maxlenght);
+            Array.Resize(ref b, maxlenght);
+            int[] result = new int[a.Length];
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                result[i] = (a[i] ^ b[i]);
+
+            }
+
+            return result;
+        }
+
+        public static int[] ShiftBitsToRight(int[]a,int step)
+        {
+            int[] result = new int[a.Length];
+
+
+            return result;
+        }
     }
 }
