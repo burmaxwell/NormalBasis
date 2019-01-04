8537d0c6c8044b899d97fa5a30c8fc6c1a5337eb
administrator <burmax98@gmail.com>
2019-01-04 13:26:40 +0200

    Mul

--- NormalBasis/Field.cs
+++ NormalBasis/Field.cs
@@ -6,43 +6,6 @@ using System.Threading.Tasks;
 
 namespace NormalBasis
 {
-    /*class Matrix
-    {
-        public static int[,] LambdaMatrix(int m)
-        {
-            int p = 2 * m + 1;
-            int[,] Matrix = new int[m, m];
-            int[] pow_two_mod_p = new int[m];
-            pow_two_mod_p[0] = 1;
-
-            for (int i = 1; i < m; i++)
-                pow_two_mod_p[i] = (pow_two_mod_p[i - 1] * 2) % p;
-
-            int pow_i, pow_j;
-
-            for (int i = 0; i < m; i++)
-            {
-                pow_i = pow_two_mod_p[i];
-
-                for (int j = 0; j < m; j++)
-                {
-                    pow_j = pow_two_mod_p[j];
-
-                    if (((pow_i + pow_j) % p) == 1 || (((pow_i - pow_j) + p) % p) == 1 || ((pow_j - pow_i + p) % p) == 1 || (((-pow_i - pow_j) + p) % p) == 1)
-
-                    {
-                        Matrix[i, j] = 1;
-                        continue;
-                    }
-
-                    Matrix[i, j] = 0;
-                }
-            }
-
-            return Matrix;
-        }
-    }
-    */
     class Field
     {
         public static int[] String_To_Byte(string stringbitvector)
